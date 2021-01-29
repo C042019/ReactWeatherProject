87e64fa0b260b4cc94fe74f1b2a9d3d5f196d232
C042019 <69688404+C042019@users.noreply.github.com>
2021-01-29 01:13:30 -0600

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Footer() {
+  return (
+    <p className="footer">
+      {" "}
+      <a href="https://github.com/C042019">Open Sourced Code</a> by Caylee
+      Bradley
+    </p>
+  );
+}
